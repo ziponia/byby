7a2a23f1f3aa23bf57ad1b40b9ee353f0e67288d
thtjwls <thtjwls@gmail.com>
2020-11-18 23:03:09 +0900

    wip상품 상세

--- src/components/ui/common/ImageSlider/ImageSlider.tsx
+++ src/components/ui/common/ImageSlider/ImageSlider.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components"
 import { CarouselProvider, Slider, Slide } from "pure-react-carousel"
 import { ImageSliderItem } from "./ImageSliderItem"
 import SlideButton from "./SlideButton"
-import { FiArrowLeftCircle, FiArrowRightCircle } from "@/components/Icon"
 
 import "pure-react-carousel/dist/react-carousel.es.css"
 
@@ -11,34 +10,44 @@ export const ImageSliderContainer = styled.div``
 
 export type ImageSliderProps = {
   imageUrls?: string[]
+  naturalSlideWidth?: number
+  naturalSlideHeight?: number
 }
 
-export const ImageSlider: React.FC<ImageSliderProps> = ({ imageUrls }) => {
+export const ImageSlider: React.FC<ImageSliderProps> = ({
+  imageUrls,
+  naturalSlideWidth,
+  naturalSlideHeight,
+}) => {
   return (
-    <CarouselProvider
-      naturalSlideWidth={1920}
-      naturalSlideHeight={420}
-      totalSlides={imageUrls.length}
-    >
-      <div style={{ position: "relative" }}>
-        <Slider>
-          {imageUrls.map((image, index) => (
-            <Slide key={index} index={index}>
-              <ImageSliderItem src={image} />
-            </Slide>
-          ))}
-        </Slider>
-        {imageUrls.length > 0 && (
-          <>
-            <SlideButton.Back />
-            <SlideButton.Next />
-          </>
-        )}
-      </div>
-    </CarouselProvider>
+    <div style={{ position: "relative" }}>
+      <CarouselProvider
+        naturalSlideWidth={naturalSlideWidth}
+        naturalSlideHeight={naturalSlideHeight}
+        totalSlides={imageUrls.length}
+      >
+        <div style={{ position: "relative" }}>
+          <Slider>
+            {imageUrls.map((image, index) => (
+              <Slide key={index} index={index}>
+                <ImageSliderItem src={image} />
+              </Slide>
+            ))}
+          </Slider>
+          {imageUrls.length > 0 && (
+            <>
+              <SlideButton.Back />
+              <SlideButton.Next />
+            </>
+          )}
+        </div>
+      </CarouselProvider>
+    </div>
   )
 }
 
 ImageSlider.defaultProps = {
   imageUrls: [],
+  naturalSlideWidth: 1920,
+  naturalSlideHeight: 420,
 }
--- src/pages/product/[productId].tsx
+++ src/pages/product/[productId].tsx
@@ -1,4 +1,4 @@
-import { PageTemplate } from "@/components/ui"
+import { Container, ImageSlider, PageTemplate } from "@/components/ui"
 import { KubyNextPageProps } from "@/interface/core"
 import { NextPageContext } from "next"
 import React from "react"
@@ -6,7 +6,46 @@ import React from "react"
 type Props = {}
 
 const ProductDetail: KubyNextPageProps<Props> = (props) => {
-  return <PageTemplate></PageTemplate>
+  return (
+    <PageTemplate>
+      <Container>
+        {/* 상품 타이틀 */}
+        <div style={{ paddingTop: 20 }}>
+          <h1>상품 제목</h1>
+        </div>
+
+        {/* row */}
+        <div style={{ display: "flex" }}>
+          {/* left box */}
+          <div style={{ flex: 1, height: 300 }}>
+            <ImageSlider
+              naturalSlideWidth={300}
+              naturalSlideHeight={300}
+              imageUrls={[
+                "https://via.placeholder.com/500x500.png?text=Main%20Banner-1",
+                "https://via.placeholder.com/500x500.png?text=Main%20Banner-2",
+                "https://via.placeholder.com/500x500.png?text=Main%20Banner-3",
+              ]}
+            />
+          </div>
+          {/* right box */}
+          <div style={{ flex: 1 }}>
+            {/* 세일가 */}
+            <p>32%</p>
+
+            {/* 정가 */}
+            <p>54,000원</p>
+
+            {/* 할인가 */}
+            <p>34,500원</p>
+
+            {/* 배송 */}
+            <p>3000원</p>
+          </div>
+        </div>
+      </Container>
+    </PageTemplate>
+  )
 }
 
 ProductDetail.getInitialProps = async (ctx: NextPageContext) => {
